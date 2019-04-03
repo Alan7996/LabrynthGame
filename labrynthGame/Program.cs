97452420f7e8330dc2f85f4f74c1533b7034728f
Alan7996 <my_ant1@naver.com>
2019-04-03 21:04:52 +0900

    Initalized the project

---
+++ labrynthGame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace labrynthGame
+{
+    class Room
+    {
+        private int width, height;
+
+        private int upDoor, downDoor, leftDoor, rightDoor;
+        private Room up, down, left, right;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            bool roomExist = (r.Next(100) > 50 ? true : false);
+
+            Room[,] Labrynth = new Room[9, 9]; /* start room (4, 4) */
+
+
+        }
+    }
+}
